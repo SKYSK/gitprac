c6fd610089929cc33191fc4122c3b1f7fea37e77
SKYSK <skysk94@gmail.com>
2022-03-03 13:04:08 +0900

    change font size and make character full-width

--- js/scl.js
+++ js/scl.js
@@ -1,4 +1,4 @@
-const sndArray = ['A','B','C','D','E','F','G','A♯/B♭','C♯/D♭','D♯/E♭','F♯/G♭','G♯/A♭']; 
+const sndArray = ['Ａ','Ｂ','Ｃ','Ｄ','Ｅ','Ｆ','Ｇ','Ａ♯/Ｂ♭','Ｃ♯/Ｄ♭','Ｄ♯/Ｅ♭','Ｆ♯/Ｇ♭','Ｇ♯/Ａ♭']; 
 const randomInt = (max) => {return Math.floor(Math.random() * max);}
 const randomTone = (array,num) => {return array[num];};
 window.onload = function(){document.getElementById("tone").innerText = randomTone(sndArray,randomInt(12));};
