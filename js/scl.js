6ee9f6e343b7b74720eaa07bee4b6894ee9c9818
SKYSK <skysk94@gmail.com>
2022-03-09 12:05:41 +0900

    change logic and names

--- js/scl.js
+++ js/scl.js
@@ -2,15 +2,21 @@ const sevenNotesArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 const twelveNotesArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A♯/B♭', 'C♯/D♭', 'D♯/E♭', 'F♯/G♭', 'G♯/A♭'];
 
 const randomInt = (max) => { return Math.floor(Math.random() * max); }
-const randomTone = (array, num) => { return array[num]; };
+const randomNote = (array, num) => { return array[num]; };
 
-function changeTone() {
-    let element = document.getElementById("tone");
-    if (document.modeForm.mode.value == 'seven') {
-        element.innerText = randomTone(sevenNotesArray, randomInt(sevenNotesArray.length));
-    } else {
-        element.innerText = randomTone(twelveNotesArray, randomInt(twelveNotesArray.length));
+function changeNote() {
+    const noteElement = document.getElementById("note");
+    const radioElements = document.getElementsByName("notesMode");
+    for (const item of radioElements) {
+        if (item.checked) {
+            if (item.value == 'seven') {
+                noteElement.innerText = randomNote(sevenNotesArray, randomInt(sevenNotesArray.length));
+            } else {
+                noteElement.innerText = randomNote(twelveNotesArray, randomInt(twelveNotesArray.length));
+            }
+            break;
+        }
     }
 }
 
-window.onload = changeTone;
+window.onload = changeNote;
